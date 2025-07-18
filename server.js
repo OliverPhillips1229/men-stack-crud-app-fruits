0260d3f7e17092b1d58b2e878f0064dbe04bc1ff
Oliver Levine <oliversfphillips@icloud.com>
2025-07-18 10:53:34 -0700

    added new.ejs file for adding new fruits

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ mongoose.connect(process.env.MONGODB_URI)
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB at ${mongoose.connection.name}`);
 });
+
 const Fruit = require('./models/fruit.js');
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
+});
+
+app.get("/fruits/new", (req, res) => {
+  res.send("This route sends the user a form page!");
 });
 
 app.listen(3000, () => {
