6efaeb3d01e55ba582c90e88ee1e5acb673bdf5e
Oliver Levine <oliversfphillips@icloud.com>
2025-07-18 09:29:51 -0700

    more setup

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+
+
 const express = require('express');
 
 const app = express();
 
 
+// GET /
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
