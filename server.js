fe002a9e1401bf22e476add1765859333ff41755
Oliver Levine <oliversfphillips@icloud.com>
2025-07-18 09:39:25 -0700

    server.js changed index.ejs changed

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-
-
 const express = require('express');
 
 const app = express();
 
 
-// GET /
 app.get("/", async (req, res) => {
-  res.render("index.ejs");
+  res.render('index.ejs');
 });
 
 
