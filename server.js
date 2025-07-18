b1abc5d954b4f8ab39a2568d3533f1fdda2a835d
Oliver Levine <oliversfphillips@icloud.com>
2025-07-18 09:56:59 -0700

    server.js updated with connection to MongoDB using Mongoose

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+const mongoose = require('mongoose');
+
+// server.js
 
 const app = express();
 
-
-app.get("/", async (req, res) => {
-  res.render('index.ejs');
+mongoose.connect(process.env.MONGODB_URI);
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Listening on port 3000");
 });
 
