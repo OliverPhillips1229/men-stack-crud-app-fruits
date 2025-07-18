51ee1044be21b3f2373d3f0849fc64e1b777c594
Oliver Levine <oliversfphillips@icloud.com>
2025-07-18 10:15:00 -0700

    Connected to MongoDB, added .env file, and updated index.ejs

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ dotenv.config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-// server.js
-
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI);
-// log connection status to terminal on start
-mongoose.connection.on("connected", () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB at ${mongoose.connection.name}`);
+});
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
 });
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log('Server is running on port 3000');
 });
 
