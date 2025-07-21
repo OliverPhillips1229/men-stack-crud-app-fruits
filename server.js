a8b5619a763ec316206f828a4e0a6666a6118755
Oliver Levine <oliversfphillips@icloud.com>
2025-07-21 08:59:04 -0700

    add more styling to CSS file

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI)
+// ✅ Serve static files from the public folder
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB at ${mongoose.connection.name}`);
@@ -19,25 +21,21 @@ app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
+app.get('/fruits', async (req, res) => {
+  const allFruits = await Fruit.find({});
+  res.render('fruits/index.ejs', { allFruits });
+});
+
 app.get("/fruits/new", (req, res) => {
   res.render('fruits/new.ejs');
 });
 
-// POST route to create a new fruit
 app.post('/fruits', async (req, res) => {
-  if (req.body.isReadyToEat === 'on') {
-    req.body.isReadyToEat = true;
-  } else {
-    req.body.isReadyToEat = false;
-  }
+  req.body.isReadyToEat = req.body.isReadyToEat === 'on';
   await Fruit.create(req.body);
   res.redirect('/fruits/new');
 });
 
-
-
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
-
-
